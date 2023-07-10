cf219f4ccfd8c6eccb02b91809129169bb249851
Godwin Opara <godwinopara62@yahoo.com>
2023-07-10 16:25:56 +0100

    implement deleting of client on the frontend

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,29 @@ import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 import Header from "./components/Header";
 import Client from "./components/Client";
 
+const cache = new InMemoryCache({
+	typePolicies: {
+		Query: {
+			fields: {
+				clients: {
+					merge(existing, incoming) {
+						return incoming;
+					},
+				},
+				projects: {
+					merge(existing, incoming) {
+						return incoming;
+					},
+				},
+			},
+		},
+	},
+});
+
 function App() {
 	const client = new ApolloClient({
 		uri: "http://localhost:5000/graphql",
-		cache: new InMemoryCache(),
+		cache: cache,
 	});
 
 	return (
--- client/src/components/ClientRow.jsx
+++ client/src/components/ClientRow.jsx
@@ -1,12 +1,30 @@
+import { gql, useMutation } from "@apollo/client";
 import { FaTrash } from "react-icons/fa";
+import { DELETE_CLIENT } from "../mutations/clientMutations";
+import { GET_CLIENTS } from "../queries/clientQueries";
+
 const ClientRow = ({ client }) => {
+	const [deleteClient] = useMutation(DELETE_CLIENT, {
+		update(cache, { data: { deleteClient } }) {
+			const { clients } = cache.readQuery({ query: GET_CLIENTS });
+			cache.writeQuery({
+				query: GET_CLIENTS,
+				data: { clients: clients.filter((client) => client.id !== deleteClient.id) },
+			});
+		},
+	});
+
+	const handleClickDelete = () => {
+		deleteClient({ variables: { id: client.id } });
+	};
+
 	return (
 		<tr>
 			<td>{client.name}</td>
 			<td>{client.email}</td>
 			<td>{client.phone}</td>
 			<td>
-				<button className="btn btn-danger btn-sm">
+				<button onClick={handleClickDelete} className="btn btn-danger btn-sm">
 					<FaTrash />
 				</button>
 			</td>
